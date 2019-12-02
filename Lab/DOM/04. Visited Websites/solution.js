672aa9880ad35b6843d15c5b977e73ed94931d77
kStoykow <kristiqn.stoikow11@gmail.com>
2019-12-02 17:13:17 +0200

    Finished Lab/DOM.

--- Lab/DOM/04. Visited Websites/solution.js
+++ Lab/DOM/04. Visited Websites/solution.js
@@ -1,4 +1,23 @@
-function solve() {
-
-  //TODO...
+const linksState = {
+  'Google': 2,
+  'SoftUni': 1,
+  'YouTube': 4,
+  'Wikipedia': 4,
+  'Gmail': 7,
+  'stackoverflow': 6,
 }
+
+const template = (x) => `visited ${x} times`;
+
+document.addEventListener('click', (evt) => {
+  if (evt.target == null || !evt.target.classList || !evt.target.classList.contains('linkCta')) {
+    throw new Error('Missing element.')
+  }
+
+  if (evt.target.parentNode == null) {
+    throw new Error('Missing element.');
+  }
+
+  evt.target.parentNode.querySelector('.visits')
+    .innerHTML = template(linksState[evt.target.textContent.trim()] += 1);
+})
