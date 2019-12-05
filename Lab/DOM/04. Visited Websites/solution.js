4c6c292edb540b52d4b0f26f36045024c093d367
kStoikow <kristiqn.stoikow11@gmail.com>
2019-12-05 20:20:12 +0200

    .

--- Lab/DOM/04. Visited Websites/solution.js
+++ Lab/DOM/04. Visited Websites/solution.js
@@ -1,4 +1,14 @@
 function solve() {
+  let links = document.querySelectorAll('.list-1');
 
-  //TODO...
+  document.addEventListener('click', (e) => {
+    if (e.target.parentNode.parentNode.classList.contains('link-1')) {
+      let textContent = e.target.parentNode.parentNode.contains('p').innerText;
+      let text = textContent.split(/\d+/g);
+      let count = Number(textContent.match(/\d+/g)[0]);
+
+      e.target.parentNode.parentNode.querySelector('p').innerText = text.join(`${count + 1}`)
+    }
+
+  })
 }
