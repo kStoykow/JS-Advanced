243465624c5035d9a12bfd34a0710fc228cf26de
kStoykow <kristiqn.stoikow11@gmail.com>
2020-01-27 20:49:00 +0200

    Adding DOM/Exercise solutions.5

--- Exercises/DOM/04. Numpad-Calculator/solution.js
+++ Exercises/DOM/04. Numpad-Calculator/solution.js
@@ -1,4 +1,67 @@
-function solve() {
-
-    //TODO...
+const calc = {
+    '+': (a, b) => (Number(a) + Number(b)).toFixed(5),
+    '-': (a, b) => (Number(a) - Number(b)).toFixed(5),
+    '*': (a, b) => (Number(a) * Number(b)).toFixed(5),
+    '/': (a, b) => (Number(a) / Number(b)).toFixed(5),
 }
+
+function solve() {
+    let keys = Array.from(document.getElementsByClassName('keys'));
+    let expression = document.getElementById('expressionOutput');
+    let resultOutput = document.getElementById('resultOutput');
+    let clear = document.querySelector('.clear');
+
+    if (expression === null || resultOutput === null || clearBtnHandler === null || keys === null) {
+        throw new Error('Missing element.');
+    }
+
+    let x = '';
+    let y = '';
+    let operand = '';
+
+    function clearBtnHandler() {
+        expression.textContent = '';
+        resultOutput.textContent = '';
+        x = '';
+        y = '';
+        operand = '';
+    }
+    function validateOperands(x, y) {
+        if (x !== '' && y !== '') {
+            return resultOutput.textContent = calc[operand](x, y);
+        }
+        return resultOutput.textContent = 'NaN';
+    }
+    function operandsHandler(currKey, operand) {
+        if (operand == '') {
+            return x += currKey;
+        }
+        return y += currKey;
+    }
+    function calcHandler(evt) {
+        const currKey = evt.target.value;
+
+        if (isNaN(Number(currKey)) === false || currKey === '.') {
+            operandsHandler(currKey, operand);
+            expression.textContent += currKey;
+        }
+
+        if (typeof calc[currKey] === 'function') {
+            operand = currKey;
+            expression.textContent += ` ${operand} `;
+        }
+
+        if (currKey === '=') {
+            validateOperands(x, y);
+        }
+    }
+
+
+    clear.addEventListener('click', clearBtnHandler);
+
+    keys.map(key => {
+        key.addEventListener('click', calcHandler);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', solve);
--- Exercises/DOM/05. Order-the-Names/solution.js
+++ Exercises/DOM/05. Order-the-Names/solution.js
@@ -1,4 +1,28 @@
+"use strict";
 function solve() {
+    let input = document.querySelector('article input');
+    let button = document.querySelector('article button');
+    let ol = document.querySelector('div ol');
+    let list = Array.from(ol.getElementsByTagName('li'));
 
-    //TODO...
+    if (input === null || ol === null || button === null || list === null) {
+        throw new Error('Missing element.');
+    }
+
+    function checkIfNameExisting(name, i) {
+        if (list[i].textContent.length !== 0) {
+            return list[i].textContent += `, ${name}`;
+        }
+        return list[i].textContent = name;
+    }
+
+    function CTAHandler() {
+        let listIndex = input.value[0].toUpperCase().charCodeAt(0) - 65;
+        let name = input.value.toUpperCase()[0] + input.value.substring(1).toLowerCase();
+
+        checkIfNameExisting(name, listIndex);
+    }
+
+    button.addEventListener('click', CTAHandler);
 }
+ document.addEventListener('DOMContentLoaded', solve);
