6b09e589b283b2878bb12a906b47aef40700177a
kStoykow <kristiqn.stoikow11@gmail.com>
2020-01-29 13:01:58 +0200

    Adding solution to Exercises/DOM/06 and refactor 05.

--- Exercises/DOM/05. Order-the-Names/solution.js
+++ Exercises/DOM/05. Order-the-Names/solution.js
@@ -1,28 +1,28 @@
 "use strict";
-function solve() {
-    let input = document.querySelector('article input');
-    let button = document.querySelector('article button');
-    let ol = document.querySelector('div ol');
-    let list = Array.from(ol.getElementsByTagName('li'));
-
-    if (input === null || ol === null || button === null || list === null) {
-        throw new Error('Missing element.');
-    }
-
-    function checkIfNameExisting(name, i) {
-        if (list[i].textContent.length !== 0) {
-            return list[i].textContent += `, ${name}`;
-        }
-        return list[i].textContent = name;
-    }
-
-    function CTAHandler() {
+function clickHandler(input, list) {
+    return function CTAHandler() {
         let listIndex = input.value[0].toUpperCase().charCodeAt(0) - 65;
         let name = input.value.toUpperCase()[0] + input.value.substring(1).toLowerCase();
 
-        checkIfNameExisting(name, listIndex);
+        checkIfNameExisting(name, listIndex, list);
+    }
+}
+function checkIfNameExisting(name, i, list) {
+    if (list[i].textContent.length > 0) {
+        return list[i].textContent += `, ${name}`;
+    }
+    return list[i].textContent = name;
+}
+
+function solve() {
+    let input = document.querySelector('article input');
+    let button = document.querySelector('article button');
+    let list = document.querySelector('div ol').querySelectorAll('li');
+
+    if (input === null || button === null || list === null) {
+        throw new Error('Missing element.');
     }
 
-    button.addEventListener('click', CTAHandler);
+    button.addEventListener('click', clickHandler(input, list, checkIfNameExisting));
 }
- document.addEventListener('DOMContentLoaded', solve);
+document.addEventListener('DOMContentLoaded', solve);
--- Exercises/DOM/06. Table-Search-Engine/solution.js
+++ Exercises/DOM/06. Table-Search-Engine/solution.js
@@ -1,3 +1,30 @@
-function solve() {
-   //TODO...
+function clickHandler(input, rows, newSearchMatcher) {
+   return function ctaHandler() {
+      let searchVal = new RegExp(input.value, 'gi');
+
+      Array.from(rows).map(x => newSearchMatcher(x, searchVal));
+      input.value = '';
+   };
 }
+
+function newSearchMatcher(row, str) {
+   row.className = '';
+
+   if (row.innerHTML.match(str)) {
+      return row.className = 'select';
+   }
+}
+
+function solve() {
+   let searchCta = document.getElementById('searchBtn');
+   let input = document.getElementById('searchField');
+   let rows = document.querySelector('.container tbody').querySelectorAll('tr');
+
+   if (searchCta === null || input === null || rows === null) {
+      throw new Error('Missing element.');
+   }
+
+   searchCta.addEventListener('click', clickHandler(input, rows, newSearchMatcher));
+}
+
+document.addEventListener('DOMContentLoaded', solve);
