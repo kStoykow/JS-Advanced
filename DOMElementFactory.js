bde3d21926f321e6c4e6479d72e3107ce79f1a12
kStoykow <kristiqn.stoikow11@gmail.com>
2022-07-03 11:01:35 +0300

    Expanding factory functionality.

--- DOMElementFactory.js
+++ DOMElementFactory.js
@@ -1,20 +1,33 @@
 function DOMElementFactory(type, content, attribute) {
     const elem = document.createElement(type);
-
     if (typeof content == 'string') {
         if (type == 'img') {
             elem.src = content;
         } else {
             elem.textContent = content;
         }
-
     } else {
         if (Array.isArray(content)) {
-            content.forEach(e => elem.appendChild(e));
+            content.forEach(e => {
+                if (typeof e == 'string') {
+                    elem.textContent = e;
+                } else {
+                    elem.appendChild(e)
+                }
+            });
+        } else {
+            elem.appendChild(content);
         }
     }
     if (attribute !== undefined) {
-        attribute.forEach(([k, v]) => elem[k] = v);
+        attribute.forEach(([k, v]) => {
+            if (k.includes('on')) {
+                const event = k.split('on')[1].toLocaleLowerCase();
+                elem.addEventListener(event, v);
+            } else {
+                elem[k] = v
+            }
+        });
     }
 
     return elem;
